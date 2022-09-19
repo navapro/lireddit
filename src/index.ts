a09bd636ee34042851b094d914c983e8d01cabcf
Naveen <navaneethk002@gmail.com>
2022-09-19 15:31:06 -0500

    initialized mikrorm
    created post object and imported the required things to init Mikroorm

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
+import {MikroORM} from '@mikro-orm/core'
+import { __prod__ } from './constants';
+import { Post } from './entities/Post';
+
+
+const main = async () => {
+    const orm = await MikroORM.init( {
+        entities: [Post],
+        dbName: 'lireddit',
+        type: 'postgresql',
+        debug: !__prod__,
+    });
+    
+
+}
+
 console.log("hello world");
